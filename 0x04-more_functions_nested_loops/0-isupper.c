f7c16e8d0c52723296341386d04c1df27c25c01a
brizyblink <Ositadinmagodspower@gmail.com>
2023-02-23 14:51:34 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,11 @@
-#include "main"
+#include "holberton.h"
 
 /**
- *  _isupper - uppercase letters
- *  @c: char to check
- *
- *  Return: 0 or 1
+   *_isupper - check for uppercase character
+    *@c: The character to print
+     *
+  * Return: 1 if "c" is uppercase and 0 otherwise
  */
-
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')

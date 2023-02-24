d5065be3cd625c79e9f3fdde4f843e873fe96803
brizyblink <Ositadinmagodspower@gmail.com>
2023-02-23 22:36:25 -0800

    3-print_remaining_days.c

--- 0x04-more_functions_nested_loops/2-main.c
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -1,14 +1,23 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
-* main - check the code
-*
-* Return: Always 0.
+* main - prints the largest of 3 integers
+* Return: 0
 */
+
 int main(void)
 {
-printf("%d\n", mul(98, 1024));
-printf("%d\n", mul(-402, 4096));
+int a, b, c;
+int largest;
+
+a = 972;
+b = -98;
+c = 0;
+
+largest = largest_number(a, b, c);
+
+printf("%d is the largest number\n", largest);
+
 return (0);
 }
